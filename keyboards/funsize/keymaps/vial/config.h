94062aec1177cfd80805a7f715beb9261498a9a4
D0hnut5 <wmmillar@gmail.com>
2023-08-27 12:29:10 -0500

    new file:   keyboards/funsize/keymaps/vial/config.h 	modified:   keyboards/funsize/keymaps/vial/rules.mk 	modified:   keyboards/funsize/keymaps/vial/vial.json

---
+++ keyboards/funsize/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+// Vial
+#define VIAL_KEYBOARD_UID {0xFA, 0x48, 0xCC, 0xC9, 0x96, 0x4F, 0x0F, 0x45}
+
+#define VIAL_COMBO_ENTRIES 12
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
