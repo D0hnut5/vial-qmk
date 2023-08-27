633372e043c2c3b0ba4cb275ec216f51ee431814
D0hnut5 <wmmillar@gmail.com>
2023-08-27 10:19:40 -0500

    new file:   keyboards/funsize/config.h 	new file:   keyboards/funsize/info.json 	new file:   keyboards/funsize/keymaps/default/keymap.c 	new file:   keyboards/funsize/keymaps/default/rules.mk 	new file:   keyboards/funsize/keymaps/vial/keymap.c 	new file:   keyboards/funsize/keymaps/vial/rules.mk 	new file:   keyboards/funsize/keymaps/vial/vial.json 	new file:   keyboards/funsize/readme.md 	new file:   keyboards/funsize/rules.mk

---
+++ keyboards/funsize/config.h
@@ -0,0 +1,29 @@
+// Copyright 2023 D0hnut5 (@D0hnut5)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define SPLIT_USB_DETECT
+#define EE_HANDS
+
+/*#define ENCODERS_PAD_A { B5, B6 }
+#define ENCODERS_PAD_B { B4, B2 }
+#define ENCODERS_PAD_A_RIGHT { B3, E6 }
+#define ENCODERS_PAD_B_RIGHT { B2, B4 }
+*/
